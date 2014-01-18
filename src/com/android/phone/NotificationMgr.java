3764bde631f55c55e4d4fc6507fadbe437f010f8
garwedgess <garethwilliams21@gmail.com>
2014-01-18 22:15:18 +0530

    [3/6] Breathing missedcall/sms/voicemail
    Updated for Marshmallow by Nick
    
    Change-Id: I7f78d37561708362e0002edbdbbec63d0279db3e
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- src/com/android/phone/NotificationMgr.java
+++ src/com/android/phone/NotificationMgr.java
@@ -35,6 +35,7 @@ import android.os.UserHandle;
 import android.os.UserManager;
 import android.preference.PreferenceManager;
 import android.provider.ContactsContract.PhoneLookup;
+import android.provider.Settings;
 import android.telecom.PhoneAccount;
 import android.telecom.PhoneAccountHandle;
 import android.telecom.TelecomManager;
@@ -241,6 +242,7 @@ public class NotificationMgr {
      *
      * @param visible true if there are messages waiting
      */
+
     /* package */ void updateMwi(int subId, boolean visible) {
         updateMwi(subId, visible, true /* enableNotificationSound */);
     }
@@ -286,6 +288,7 @@ public class NotificationMgr {
         Log.i(LOG_TAG, "updateMwi(): subId " + subId + " update to " + visible);
         mMwiVisible.put(subId, visible);
 
+        int resId;
         if (visible) {
             if (phone == null) {
                 Log.w(LOG_TAG, "Found null phone for: " + subId);
@@ -296,11 +299,17 @@ public class NotificationMgr {
             if (subInfo == null) {
                 Log.w(LOG_TAG, "Found null subscription info for: " + subId);
                 return;
-            }
 
-            int resId = android.R.drawable.stat_notify_voicemail;
-            if (mTelephonyManager.getPhoneCount() > 1) {
+            }
+          
+            if (Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.KEY_VOICEMAIL_BREATH, 0) == 1) {
+                resId = R.drawable.stat_notify_voicemail_breath;
+            } else {
+    			resId = android.R.drawable.stat_notify_voicemail;
+            	if (mTelephonyManager.getPhoneCount() > 1) {
                 resId = mwiIcon[phoneId];
+       			}
             }
 
             // This Notification can get a lot fancier once we have more
