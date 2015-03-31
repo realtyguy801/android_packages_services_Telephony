10d3f4c3992e5f5575b7028b21206bfc47a02502
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-03-31 16:06:25 +0100

    Telephony: make call waiting option a switch preference
    Change-Id: Idb46fbe8a338c8ae6df3e5c218cfc5c0921327fe

--- src/com/android/phone/GsmUmtsAdditionalCallOptions.java
+++ src/com/android/phone/GsmUmtsAdditionalCallOptions.java
@@ -92,7 +92,7 @@ public class GsmUmtsAdditionalCallOptions extends
             mInitIndex++;
             Preference pref = mPreferences.get(mInitIndex);
             if (pref instanceof CallWaitingSwitchPreference) {
-                ((CallWaitingSwitchPreference) pref).init(this, false, mPhoneId);
+                ((CallWaitingSwitchPreference) pref).init(this, false, mPhone);
             } else if (pref instanceof MSISDNEditPreference) {
                 ((MSISDNEditPreference) pref).init(this, false, mPhoneId);
             }
